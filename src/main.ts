9f1699f1f67f2aa1de6d0032cd249aa219a31f48
Alexander Lazarev <xxragepowerxxxxragepowerxx@gmail.com>
2025-12-23 21:09:13 +0100

    refactor: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const frontendUrl = process.env.FRONTEND_URL;
+
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://127.0.0.1:3001'],
+    origin: frontendUrl
+      ? [frontendUrl]
+      : ['http://localhost:3001', 'http://127.0.0.1:3001'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: false,
